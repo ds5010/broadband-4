cfaf8bf080b463af81eefdc06645c4019ddc6e6a
Joe Schweiss <joschw@Joes-MacBook-Pro.local>
2023-04-21 19:30:56 -0400

    added scores to json title

--- src/maps.py
+++ src/maps.py
@@ -23,7 +23,8 @@ for score in scores:
     df_data=pd.read_csv('data/'+score+'_tract.csv')
     del df_data[df_data.columns[0]]
     df_data['county']=df_data['TractID'].apply(lambda x:str(x)[2:5]+str(x)[-4:])
-    df_data['title']=df_data['county'].apply(lambda x: diction[x[:3]]['name']+" tract:"+x[-4:] if x[:3] in diction else NA)
+    df_data['title']=df_data['county'].apply(lambda x: diction[x[:3]]['name']+"\ntract:"+x[-4:] if x[:3] in diction else NA)
+    df_data['title']=df_data['title']+ "\n"+df_data[score].astype(str)
     df_data['GEOID']=df_data['TractID'].astype('str')
     df_data.drop(columns=['TractID'], inplace=True)
     new_gdf = gdf.merge(df_data[['GEOID',score,'title']],on='GEOID', how='left')
