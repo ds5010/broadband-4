d9324160c2333a689683cf495e4c70991afd5b3c
Jacob Gordon <jgordon2@bobcat-109-30.bates.edu>
2024-04-15 19:54:09 -0400

    updated shapefile and merge

--- src/create_json.py
+++ src/create_json.py
@@ -2,16 +2,17 @@
 import geopandas as gpd
 import pandas as pd
 
-def create_json(filename='docs/tracts.json'):
+def create_json(filename='docs/counties.json'):
     # Create dataframes
-    geo_data = gpd.read_file("data/tl_2019_23_tract.zip").query('ALAND > 0') # Removes water from maps
-    de_data = pd.read_excel("data/county_tract_total_covered_populations.xlsx", sheet_name='tract_total_covered_populations')
+    geo_data = gpd.read_file("https://www2.census.gov/geo/tiger/TIGER2019/COUNTY/tl_2019_us_county.zip")
+    de_data = pd.read_excel("data/county_tract_total_covered_populations.xlsx", sheet_name='county_total_covered_population')
 
     # Clean and merge
     de_data = de_data[de_data['geo_id'].astype(str).str[:2] == '23'] # Extracting only maine data
-    de_data['geo_id'] = de_data['geo_id'].astype(str)
-    combined_data = geo_data.merge(de_data, left_on='GEOID', right_on='geo_id')
-    combined_data['geo_id'] = combined_data['geo_id'].astype(int)
+    de_data['GEOID'] = de_data['geo_id'].astype(str)
+    geo_data['GEOID'] = geo_data['GEOID'].str[:5]
+    de_data['GEOID'] = de_data['GEOID'].str[:5]
+    combined_data = geo_data.merge(de_data, on='GEOID')
 
     # Download as geojson in docs directory
     combined_data.to_file(filename, driver='GeoJSON')
